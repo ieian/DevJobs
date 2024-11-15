07f3d11c7459cde034f098a44814f1e9fd6fe8f1
ieian <yeyotadeoae@gmail.com>
2024-11-15 11:37:05 -0700

    222 - error catcher

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -9,16 +9,33 @@ exports.formCrearCuenta = (req, res) => {
     })
 }
 
-//volver al video 220 para terminar esto si es necesario
-exports.validarRegistro = (req, res, next) => {
-    req.checkBody('nombre', 'El nombre es obligatorio').noEmpty();
+exports.validarRegistro = [
+    body('nombre').notEmpty().withMessage('El Nombre es obligatorio').escape(),
+    body('email').isEmail().withMessage('El email debe ser válido').escape(),
+    body('password').notEmpty().withMessage('El password no puede ir vacío').escape(),
+    body('confirmar').notEmpty().withMessage('Confirmar password no puede ir vacío').escape(),
+    body('confirmar').custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error('El password es diferente');
+        }
+        return true;
+    }),
 
-    const errores = res.vadationErrors();
+    (req, res, next) => {
+        const errores = validationResult(req);
+        if (!errores.isEmpty()) {
+            req.flash('error', errores.mapped(error => error.msg));
 
-    console.log(errores);
-
-    return;
-}
+            res.render('crear-cuenta', {
+                nombrePagina: 'Crea tu cuenta en devJobs',
+                tagline: 'Comienza a publicar tus vacantes gratis, solo debes crear una cuenta',
+                mensajes: req.flash()
+            });
+            return;
+        }
+        next(); 
+    }
+];
 
 exports.crearUsuario = async (req, res, next) => {
     const usuario = new Usuarios(req.body);
