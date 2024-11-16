0abafd745b16ed17b76edace7d87d007b7176f31
ieian <yeyotadeoae@gmail.com>
2024-11-15 19:07:14 -0700

    232 - tus vacantes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 const passport = require('passport');
+const mongoose = require('mongoose');
+const Vacante = mongoose.model('Vacante');
 
 exports.autenticarUsuario = passport.authenticate('local',{
     successRedirect : '/administracion',
@@ -15,9 +17,12 @@ exports.verificarUsuario = (req, res, next) => {
     res.redirect('/iniciar-sesion');
 };
 
-exports.mostrarPanel = (req, res) => {
+exports.mostrarPanel = async (req, res) => {
+    const vacantes = await Vacante.find({ autor: req.user._id }).lean();;
+
     res.render('administracion', {
         nombrePagina : 'Panel de Administracion',
-        tagline : 'Crea y administra tus vacantes desde aqui'
+        tagline : 'Crea y administra tus vacantes desde aqui',
+        vacantes
     })
 };
