1ed8bcacd8204d6c974034e4693f6b075d586e52
ieian <yeyotadeoae@gmail.com>
2024-11-16 20:15:46 -0700

    257 - configuraciones iniciales para envio de correos

---
+++ config/email.js
@@ -0,0 +1,6 @@
+module.exports = {
+    user: 'b3fc69aa45e7d3',
+    pass: '3002482f641ed9',
+    host: 'sandbox.smtp.mailtrap.io',
+    port: '2525',
+}
---
+++ handlers/email.js
@@ -0,0 +1,11 @@
+const emailConfig = require('../config/email');
+const nodemailer = require('nodemailer');
+
+let transport = nodemailer.createTransport({
+    host : emailConfig.host,
+    port : emailConfig.port,
+    auth: {
+        user : emailConfig.user,
+        pass: emailConfig.pass
+    }
+});
